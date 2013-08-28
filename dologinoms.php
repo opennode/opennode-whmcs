64df46d61b90c93b052b629d996d9e818360b535
Ilja Livenson <ilja.livenson@gmail.com>
2013-08-28 12:15:39 +0200

    Remove php notices about undefined constants (LEV-99)

--- dologinoms.php
+++ dologinoms.php
@@ -2,10 +2,10 @@
 include_once (dirname(__FILE__) . '/includes/hooks/inc/oms_utils.php');
 global $oms_hostname, $oms_pass;
 
-$token = $_POST[token];
-$username = $_POST[username];
-$password = $_POST[password];
-$_COOKIE[p] = $password;
+$token = $_POST['token'];
+$username = $_POST['username'];
+$password = $_POST['password'];
+$_COOKIE['p'] = $password;
 
 setcookie("p", $password);
 header('Location: dologin.php?token=' . $token . '&username=' . $username . '&password=' . $password);
