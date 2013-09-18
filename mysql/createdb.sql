959c93cfa33157a93a06ce708e6d57d606233e76
Ilja Livenson <ilja.livenson@gmail.com>
2013-09-18 14:36:34 -0700

    Set OMS_USAGE schema to use current time as a default value (LEV-136)

--- mysql/createdb.sql
+++ mysql/createdb.sql
@@ -8,7 +8,7 @@ GRANT ALL on OMS_USAGE.* to 'username'@'oms-vm-ip' IDENTIFIED BY 'password';
 CREATE TABLE CONF_CHANGES(
     id INT NOT NULL AUTO_INCREMENT,
     username VARCHAR(20) NOT NULL,
-    timestamp TIMESTAMP NOT NULL,
+    timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     cores FLOAT NOT NULL,
     disk FLOAT NOT NULL ,
     memory FLOAT NOT NULL,
