f164d942397ebc9a5972bf9f78bc733af526ca70
Ilja Livenson <ilja.livenson@gmail.com>
2013-11-04 14:46:30 +0200

    Added better debugging options for oms stats script (OMS-603)

--- scripts/vmsummary.py
+++ scripts/vmsummary.py
@@ -14,19 +14,29 @@ except ImportError:
 OMS_URL="http://oms-vm/"
 COMPUTES_INFO="/computes/?depth=1&attrs=hostname,license_activated,owner,memory,num_cores,diskspace,state,ipv4_address"
 OMS_USERNAME="opennode"
-OMS_PASSWORD="changeme"
+OMS_PASSWORD="password"
 FILENAME="vm-summary.csv"
+DEBUG=False
+
+# requests debug
+if DEBUG:
+    import httplib
+    httplib.HTTPConnection.debuglevel = 1
+
 
 def get_client_info(name):
     """Retrieve client specific info as seen by OMS"""
     r = requests.get(OMS_URL + '/home/%s?depth=1&attrs=uid,name' % name, auth=(OMS_USERNAME, OMS_PASSWORD))
-    print name
     return json.loads(r.text)
 
 # get all the known VMs
-print "About to connect to %s to get the information. If the response takes longer, please check the set credentials"
+print "About to connect to %s to get the information. If the response takes longer, please check the set credentials" % OMS_URL
 r = requests.get(OMS_URL + COMPUTES_INFO, auth=(OMS_USERNAME, OMS_PASSWORD))
-vms = json.loads(r.text)
+if r.status_code == 200:
+    vms = json.loads(r.text)
+else:
+    print "Failed to get a reasonable response from OMS. Switch on DEBUG to get more info"
+    sys.exit(2)
 
 # write down statistics into a csv file
 with open(FILENAME, 'wb') as csvfile:
@@ -49,5 +59,4 @@ with open(FILENAME, 'wb') as csvfile:
                   v['ipv4_address']]
         csvwriter.writerow(vmdata)
         print vmdata
-
 
